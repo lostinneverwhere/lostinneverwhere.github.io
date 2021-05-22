897129f4268a03037fdda9c147e7ad75016ec19c
Pankaj Ajwani <pankajbalram.ajwani@fastretailing.com>
2021-05-22 21:35:49 +0900

    add react app

---
+++ precache-manifest.3d81516464180fca0236462a0e77b29a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd34648be8265f01c338a4c78dba010",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73ed200d6a7f50f1d549",
+    "url": "/static/css/main.fa6c243e.chunk.css"
+  },
+  {
+    "revision": "10929a1925e8609326b7",
+    "url": "/static/js/2.587f8f77.chunk.js"
+  },
+  {
+    "revision": "4a05bee30832483c0b60",
+    "url": "/static/js/3.5c82a9e7.chunk.js"
+  },
+  {
+    "revision": "73ed200d6a7f50f1d549",
+    "url": "/static/js/main.3f62796a.chunk.js"
+  },
+  {
+    "revision": "a75f7d6edb603841dd1a",
+    "url": "/static/js/runtime~main.9ecb1a1c.js"
+  },
+  {
+    "revision": "15c494706df31c5ce3ee57470ac11718",
+    "url": "/static/media/img1.15c49470.png"
+  }
+]);
